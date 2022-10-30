7d3488d9c8d472164d6a564f4ea90e896dcf6479
Thomas Davidson <tomd1969@gmail.com>
2022-10-30 13:23:56 -0400

    Added some more functionality to the page and made things a little more attractive (at least, to me).

--- script.js
+++ script.js
@@ -4,17 +4,13 @@
 
 let words;
 let wordCollection = [];
-
+let solutionWord = "";
+let workingWord = "";
+const workSpace = document.getElementById("workSpace");
+const startButton = document.getElementById("startButton");
 async function setup() {
     words = await fetch("./words.json");
     words = await words.json();
-    
-}
-
-async function main() {
-    //setup the initial full word list
-    
-    await setup();
     //bust out the word list so I can refer to a word by 
     //wordCollection[wordLength][index].  I may change how this
     //works later to wordCollection being a function that returns
@@ -30,12 +26,34 @@ async function main() {
             wordCollection.push(newWords);
         }
     }
+    startButton.addEventListener('click', (e) => startButton_click(e));
+}
+
+async function main() {
+    //setup the initial full word list
+    
+    await setup();
+    //is this even necessary?
+    
+    
+}
+
+main()
+
+function removeLetters(word) {
+    const rx = /[a-z]/ig;
+    let myReturn = word.replace(rx, "_");
+    return myReturn;
+}
+
+function startButton_click(e){
     //For testing purposes, I'm going to test with wordlength 6
     //I plan a dropdown box where the user can select how long the
     //word they want
     const wordLength = 6;
     const numberOfWords = wordCollection[wordLength].length;
-    
+    let indexOfWord = Math.floor(Math.random() * numberOfWords);
+    solutionWord = wordCollection[wordLength][indexOfWord];
+    workingWord = removeLetters(solutionWord);
+    workSpace.textContent = workingWord;
 }
-
-main()
